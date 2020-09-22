09137617fef35fe639e1bfd9fad0cc28917b54ee
prescottbreeden <prescott.breeden@gmail.com>
2020-09-21 20:18:25 -0700

    cleanup

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,13 +1,16 @@
-import { User } from "../entities/User";
-import { DbContext, UsernameInput, UserResponse } from "../types";
-import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
-import { RegisterValidations, UserLoginValidation, UserRegisterValidation } from "../validations/UserValidation";
-import {createAPIErrors} from "../util/utilities";
+import { User } from '../entities/User';
+import { DbContext, UsernameInput, UserResponse } from '../types';
+import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
+import {
+  RegisterValidations,
+  UserLoginValidation,
+  UserRegisterValidation,
+} from '../validations/UserValidation';
+import { createAPIErrors } from '../util/utilities';
 import argon2 from 'argon2';
 
 @Resolver()
 export class UserResolver {
-
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernameInput,
@@ -16,7 +19,7 @@ export class UserResolver {
     const data: RegisterValidations = {
       ...options,
       exists: await db.findOne(User, { username: options.username }),
-    }
+    };
 
     // validate user registration
     const v = UserRegisterValidation();
@@ -25,16 +28,16 @@ export class UserResolver {
     return !valid
       ? { errors: createAPIErrors(v.validationState) }
       : (async () => {
-        const hash = await argon2.hash(data.password);
-        const user = db.create(User, {
-          username: data.username, 
-          password: hash
-        });
-        await db.persistAndFlush(user);
-        return {
-          user
-        };
-      })();
+          const hash = await argon2.hash(data.password);
+          const user = db.create(User, {
+            username: data.username,
+            password: hash,
+          });
+          await db.persistAndFlush(user);
+          return {
+            user,
+          };
+        })();
   }
 
   @Mutation(() => UserResponse)
@@ -47,10 +50,13 @@ export class UserResolver {
 
     // validate user registration
     const v = UserLoginValidation();
-    const valid = v.validateCustom([
-      { key: 'username', value: username, state: user },
-      { key: 'password', value: password, state: user },
-    ], user);
+    const valid = v.validateCustom(
+      [
+        { key: 'username', value: username, state: user },
+        { key: 'password', value: password, state: user },
+      ],
+      user
+    );
 
     return !valid
       ? { errors: createAPIErrors(v.validationState) }
